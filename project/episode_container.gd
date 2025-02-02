8bb913e6298a10bd9f58f655e47252ff7f04e949
Vivian <68966813+VivianVerdant@users.noreply.github.com>
2025-02-01 20:25:35 -0600

    scroll to top

--- project/episode_container.gd
+++ project/episode_container.gd
@@ -2,14 +2,24 @@ extends ScrollContainer
 
 func _input(event: InputEvent) -> void:
 	
-	if event is InputEventScreenTouch and not %bottom_menu.visible:
-		%bottom_menu.visible = !%bottom_menu.visible
+	if event is InputEventScreenTouch and event.pressed and not %bottom_menu.visible:
+		bottom_menu_fade_in()
 	if event is InputEventMouseButton and not %bottom_menu.visible:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			%bottom_menu.visible = !%bottom_menu.visible
+			bottom_menu_fade_in()
 	
 	if event is InputEventScreenDrag:
-		%bottom_menu.visible = false
+		bottom_menu_fade_out()
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP or event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
-			%bottom_menu.visible = false
+			bottom_menu_fade_out()
+
+func bottom_menu_fade_in():
+	%bottom_menu.set_visible(true)
+	var tween = create_tween()
+	tween.tween_property(%bottom_menu, "modulate", Color(1.0,1.0,1.0,1.0), .1)
+	
+func bottom_menu_fade_out():
+	var tween = create_tween()
+	tween.tween_property(%bottom_menu, "modulate", Color(1.0,1.0,1.0,0.0), .25)
+	tween.tween_property(%bottom_menu, "visible", false, .25)
--- project/main_container.gd
+++ project/main_container.gd
@@ -13,6 +13,13 @@ extends PanelContainer
 func _on_prev_button_up() -> void:
 	print("prev")
 
-
 func _on_next_button_up() -> void:
 	print("next")
+
+func _on_scroll_top_button_up() -> void:
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	var percent = %episode_container.scroll_vertical / %episode_container.get_v_scroll_bar().max_value
+	tween.tween_property(%episode_container, "scroll_vertical", 0.0, 4.0 * percent)
+	%episode_container.scroll_vertical
